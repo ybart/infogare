c6168506b4ebcff34259a4d60ef504f9ee6123d5
Yvan BARTHÉLEMY <y.barthelemy@linkbynet.com>
2019-02-02 12:55:10 +0100

    Add moment.js clock

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 var moment = require('moment');
+var $ = require('jquery');
+
 moment().format();
+
+var updateClock = function() {
+  $('.clock-time').html(moment().format('HH:mm'));
+  $('.seconds').html(moment().format('ss'));
+
+  var timeout = 1000 - (new Date()).getMilliseconds();
+  setTimeout(updateClock, timeout);
+};
+
+$(function() {
+  updateClock();
+});
